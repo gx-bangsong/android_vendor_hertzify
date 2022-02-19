d849a4996e28ed868dce988af06be607cc463246
jhonboy121 <alfredmathew05@gmail.com>
2022-02-19 14:46:25 +0900

    vendor: Introduce App Lock [4/4]
    Change-Id: Ie6d86e301efe14146c961b11bfccd3fa74a1562c
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -249,6 +249,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/hertzify/prebuilt/common/etc/init/init.openssh.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.openssh.rc
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/hertzify/config/permissions/privapp-permissions-hertzify.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-hertzify.xml
+
 # rsync
 PRODUCT_PACKAGES += \
     rsync
