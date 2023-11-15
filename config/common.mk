49e2a14a9c2e64ccd7d8a1becdeb3b78358e4a3e
ponces <ponces26@gmail.com>
2023-11-15 16:02:38 +0000

    common: AOSPA's face unlock implementation
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -213,6 +213,18 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.boot.wificountrycode?=00
 
+# FaceUnlock
+ifneq ($(TARGET_FACE_UNLOCK_SUPPORTED),false)
+PRODUCT_PACKAGES += \
+    FaceUnlock
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 # FRP
 PRODUCT_COPY_FILES += \
     vendor/hertzify/prebuilt/common/bin/wipe-frp.sh:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/wipe-frp
